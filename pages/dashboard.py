79bccc375c89452eefd4b83c5ffa4384f8ab568a
Younes7777 <younesiferd@gmail.com>
2025-07-17 13:43:01 +0200

    mein Lebenslauf aktualisieren

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -11,6 +11,11 @@ from dash import html
 
 layout = html.Div([
     html.H3("Diese Seite wird derzeit bearbeitet"),
+    html.H4("Power BI Bericht: Chinook Musikverkauf"),
+    html.A("Download Bericht (PBIX)", 
+           href="https://github.com/Younes7777/Chinook_Verkaufs-bersicht/blob/main/Chinook%20%E2%80%93%20Verkaufs%C3%BCbersicht.pbix",
+           target="_blank")
+
     
 ])
 
